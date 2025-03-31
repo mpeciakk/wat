a4ddbe31c8048e74ba0c496a8727acc028be5685
mpeciakk <mpeciakk@gmail.com>
2025-03-31 22:16:09 +0200

    fix: proper handling javascript dates

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -11,27 +11,29 @@ export function getCalendar(lessons: Lesson[]) {
   });
 
   lessons.forEach((lesson) => {
-    const start = new Date();
-    start.setHours(lesson.block.from[0]);
-    start.setMinutes(lesson.block.from[1]);
-    start.setSeconds(0);
-    start.setFullYear(parseInt(lesson.date[0]));
-    start.setMonth(parseInt(lesson.date[1]) - 1);
-    start.setDate(parseInt(lesson.date[2]));
+    const start = new Date(
+      parseInt(lesson.date[0]),  // year
+      parseInt(lesson.date[1]) - 1,  // month (0-based)
+      parseInt(lesson.date[2]),  // day
+      lesson.block.from[0],  // hours
+      lesson.block.from[1],  // minutes
+      0  // seconds
+    );
 
-    const end = new Date();
-    end.setHours(lesson.block.to[0]);
-    end.setMinutes(lesson.block.to[1]);
-    end.setSeconds(0);
-    end.setFullYear(parseInt(lesson.date[0]));
-    end.setMonth(parseInt(lesson.date[1]) - 1);
-    end.setDate(parseInt(lesson.date[2]));
+    const end = new Date(
+      parseInt(lesson.date[0]),  // year
+      parseInt(lesson.date[1]) - 1,  // month (0-based)
+      parseInt(lesson.date[2]),  // day
+      lesson.block.to[0],  // hours
+      lesson.block.to[1],  // minutes
+      0  // seconds
+    );
 
     cal.createEvent({
       start: start,
       end: end,
       summary: lesson.name,
-      description: `${lesson.type}\n${lesson.teacher}\n\n${lesson.data}`,
+      description: `${lesson.type}\n${lesson.teacher}\n\n${lesson.data}\n\n${lesson.date[0]}-${lesson.date[1]}-${lesson.date[2]}\n\n${parseInt(lesson.date[1]) - 1}\n\n${start}`,
     });
   });
 
